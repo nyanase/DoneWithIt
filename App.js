143dc909ae73cd7180f056bab86a03e197da9760
Nao Yanase <nao.yanase@wustl.edu>
2020-09-15 02:11:56 -0700

    Login Screen

--- App.js
+++ App.js
@@ -4,25 +4,8 @@ import Screen from "./app/components/Screen";
 import { TextInput, Text } from "react-native";
 import AppTextInput from "./app/components/AppTextInput";
 import AppPicker from "./app/components/AppPicker";
+import LoginScreen from "./app/screens/LoginScreen";
 
 export default function App() {
-  const categories = [
-    { label: "Furniture", value: 1 },
-    { label: "Clothing", value: 2 },
-    { label: "Cameras", value: 3 },
-  ];
-  const [category, setCategory] = useState(categories[0]);
-
-  return (
-    <Screen>
-      <AppPicker
-        selectedItem={category}
-        onSelectItem={(item) => setCategory(item)}
-        icon="apps"
-        placeholder="Category"
-        items={categories}
-      />
-      <AppTextInput icon="email" placeholder="Email" />
-    </Screen>
-  );
+  return <LoginScreen />;
 }
